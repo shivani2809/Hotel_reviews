8ccdaffeddf93a5dd7cc95d28454f3f74bb4537d
Shivani Chavan <147230432+shivani2809@users.noreply.github.com>
2024-02-18 01:49:21 +0530

    Update review_model.py

--- review_model.py
+++ review_model.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-! pip install scikit-learn
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 
